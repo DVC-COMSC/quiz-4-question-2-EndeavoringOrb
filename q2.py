dba63ba00e09393f69a03de2dd9b75cc36d9be3d
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-01 23:25:14 -0700

    2nd commit

--- q2.py
+++ q2.py
@@ -1,9 +1,11 @@
-string_list = []
+largest_string = ""
+smallest_string = ""
 while True:
-    string_list.append(input("Enter a string: "))
-    if string_list[len(string_list)-1]=="STOP" or string_list[len(string_list)-1]=="stop":
+    new_string = input("Enter a string: ")
+    if new_string == "STOP" or new_string == "stop":
         break
-    for i in range(len(string_list)-1,0,-1):
-        if len(string_list[i]) > len(string_list[i-1]):
-            string_list[i],string_list[i-1] = string_list[i-1],string_list[i]
-print(f"{string_list[0]} {string_list[len(string_list)-2]}")
+    if len(new_string) > len(largest_string):
+        largest_string = new_string
+    if len(new_string) < len(smallest_string):
+        smallest_string = new_string
+print(f"{largest_string} {smallest_string}")
