c3d7d26a34d8ee66d7de0ffaf5dabaaec303e942
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-01 20:11:38 -0700

    1st commit

--- q2.py
+++ q2.py
@@ -1,10 +1,9 @@
-
-# ******************************
-# Make your Code
-# ******************************
-
-
-# Requirement
-# No need to use list
-# All input values are taken one by one separatively.
-###
+string_list = []
+while True:
+    string_list.append(input("Enter a string: "))
+    if string_list[len(string_list)-1]=="STOP" or string_list[len(string_list)-1]=="stop":
+        break
+    for i in range(len(string_list)-1,0,-1):
+        if len(string_list[i]) > len(string_list[i-1]):
+            string_list[i],string_list[i-1] = string_list[i-1],string_list[i]
+print(f"{string_list[0]} {string_list[len(string_list)-2]}")
